c229557e7e8946564ebf08c71c4f432f0a2fe047
manoj30808 <manoj30808@gmail.com>
2017-06-24 23:17:35 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Address Validation in Laravel
+    ``  This package used for physical address verification  with google.
+
+    1) 
+    2) Add below line in your provider at (config\app.php) 
+        'providers' => [
+            ---------
+            Sonlabs\AddressValid\AddressValidServiceProvider::class,
+            ---------
+        ],
+    3) please add below line at (resources\lang\en\validation.php)
+        'validation.check_address' => 'The :attribute must be validate.',
+    
+    You done, now please use with below sysntax
+        In validation rules use login "check_address"
+
+        $rules = [
+            'name'     => 'required',
+            'address' => 'required|check_address',
+        ];
---
+++ src/AddressValidServiceProvider.php
@@ -0,0 +1,40 @@
+<?php namespace Sonlabs\AddressValid;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
+use GuzzleHttp\Client;
+
+class AddressValidServiceProvider extends ServiceProvider {
+
+
+	//protected $defer = false;
+
+	/**
+	 * Register the application services.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+        Validator::extend('check_address', function ($attribute, $value) {
+            if(!empty($value)){
+                $remove_special_char = preg_replace("/[^ \w]+/", "", $value);
+                $remove_spaces = str_replace(' ', '+', $remove_special_char);
+                
+                $client = new Client(['verify' => false]);
+                $result = $client->request('GET', 'https://maps.googleapis.com/maps/api/geocode/json', [
+                    'query' => ['address' => $remove_spaces]
+                ]);
+                $response = json_decode($result->getBody());    
+                if ($response->status=='OK') {
+                    return TRUE;
+                }
+                return FALSE;
+            }
+            return FALSE;
+        });
+	}
+	public function register()
+    {
+	}
+}
